b2eb90ec13c2a31ae5207044ad679b6cff040712
Fernando Mendez <ferhummes84@gmail.com>
2022-12-02 14:23:01 -0300

    Ajuste de navegação e criação da página Home

--- src/Routes/AllPages.jsx
+++ src/Routes/AllPages.jsx
@@ -14,8 +14,8 @@ return(
                 headerShown: false,
             }}
         >
-            <Stack.Screen name="AppExplanation" component={AppExplanation} />
             <Stack.Screen name="Start" component={Start} />
+            <Stack.Screen name="AppExplanation" component={AppExplanation} />
         </Stack.Navigator>
     </NavigationContainer>
 );
